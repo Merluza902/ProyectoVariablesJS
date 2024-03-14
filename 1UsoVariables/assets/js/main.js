c8108dbc26a545f46c3147c1be34bf8b9d93d2c5
merluzza <melisadiaz1234@gmail.com>
2024-03-14 11:18:09 -0300

    First change

---
+++ 1UsoVariables/assets/js/main.js
@@ -0,0 +1,18 @@
+//Pide las dos variables al usuario y las guarda en n1 y n2
+let n1 = +prompt("ingrese primer numero mayor a cero: ");
+let n2 = +prompt("Ingrese un segundo número mayor a cero: ");
+//Realiza las operaciones matemáticas respectivas
+sumar=n1+n2
+resta=n1-n2
+division=n1/n2
+mult=n1*n2
+modulo=n1%n2
+//Muestra los resultados por consola
+console.log(sumar)
+console.log(resta)
+console.log(division)
+console.log(mult)
+console.log(modulo)
+
+let resultado = `${name} tiene ${age} años y trabaja en ${ciudad}`
+document.write(resultado)
