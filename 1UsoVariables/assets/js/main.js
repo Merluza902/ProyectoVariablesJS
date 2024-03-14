ae1929ec944cf39a6a6d6e9c8b128f0491869565
merluzza <melisadiaz1234@gmail.com>
2024-03-14 12:13:27 -0300

    Add fahr and kelv exercise

--- 1UsoVariables/assets/js/main.js
+++ 1UsoVariables/assets/js/main.js
@@ -1,18 +1,30 @@
+//FIRST:
 //Pide las dos variables al usuario y las guarda en n1 y n2
 let n1 = +prompt("ingrese primer numero mayor a cero: ");
 let n2 = +prompt("Ingrese un segundo número mayor a cero: ");
 //Realiza las operaciones matemáticas respectivas
-sumar=n1+n2
-resta=n1-n2
-division=n1/n2
-mult=n1*n2
-modulo=n1%n2
+sumar=n1+n2;
+resta=n1-n2;
+division=n1/n2;
+mult=n1*n2;
+modulo=n1%n2;
 //Muestra los resultados por consola
-console.log(sumar)
-console.log(resta)
-console.log(division)
-console.log(mult)
-console.log(modulo)
+console.log(sumar);
+console.log(resta);
+console.log(division);
+console.log(mult);
+console.log(modulo);
 
-let resultado = `${name} tiene ${age} años y trabaja en ${ciudad}`
+//SECOND "CELSIUS TO KELVIN-FARENHEIT"
+//Pedir la t° en celsius
+let celsius = +prompt("Ingrese la temperatura en celsius");
+//Fórmula para transformar a Kelvin y Farenheit
+kelvin = celsius + 273.15
+farenheit = (celsius * 9/5) + 32
+console.log(kelvin)
+console.log(farenheit)
+
+
+
+let resultado = `${name} tiene ${age} años y trabaja en ${ciudad}`;
 document.write(resultado)
